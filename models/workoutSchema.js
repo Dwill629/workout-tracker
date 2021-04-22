8e691f71a8984d761b8b6c9d3b4e93fc4d9523ea
Devon <Dwill629@gmail.com>
2021-04-22 00:53:22 -0400

    added workoutSchema const

--- models/workoutSchema.js
+++ models/workoutSchema.js
@@ -1,3 +1,15 @@
 const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema(
+  {
+    day: { type: Date, default: () => new Date() },
+    exercises: Array
+  },
+  {
+    toJSON: {
+      virtuals: true
+    }
+  }
+);
