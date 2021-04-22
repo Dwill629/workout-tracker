9e871373292b7b6cb1d6c78b9632052a1730f5f9
Devon <Dwill629@gmail.com>
2021-04-22 00:49:14 -0400

    added mongoose.connect localhost

--- server.js
+++ server.js
@@ -4,3 +4,10 @@ app.use(express.static('public'));
 
 app.use(require('./routes/apiRoutes'));
 app.use(require('./routes/htmlRoutes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
